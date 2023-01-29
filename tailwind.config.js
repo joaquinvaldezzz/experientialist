1487fbc85a053dfb2f240fdd26944eb4c14d68c8
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-01-29 14:11:09 +0800

    Disable RGB color format

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ const files = require('./files')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [...files],
+  future: {
+    disableColorOpacityUtilitiesByDefault: true,
+  },
   theme: {
     container: {
       center: true,
